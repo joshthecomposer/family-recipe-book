f553e89db1f9642b6f1526f594b53eb07e0679e9
joshthecomposer <110321887+joshthecomposer@users.noreply.github.com>
2023-09-09 18:29:09 -0500

    Complete FR-4

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using MyApp.DataStorage;
+using MyApp.Models;
 
 namespace MyApp.Controllers;
 
@@ -6,11 +8,41 @@ namespace MyApp.Controllers;
 [Route("api/auth")]
 public class AuthController : ControllerBase
 {
-    [HttpPost("register")]
-    public async Task<ActionResult> Register()
+    private readonly DBContext _db;
+
+    public AuthController(DBContext db)
     {
-        await Task.Delay(1);
-        return Ok("Register");
+        _db = db;
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<User>> Register(User user)
+    {
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                await _db.Users.AddAsync(user);
+                await _db.SaveChangesAsync();
+                return user;
+            }
+            catch
+            {
+                return StatusCode(500, "Error saving resource to the database, try again.");
+            }
+        }
+        return BadRequest(ModelState);
+    }
+
+    [HttpGet("test/{id}")]
+    public async Task<ActionResult<User>> TestGetById(int id)
+    {
+        User? check = await _db.Users.FindAsync(id);
+        if (check != null)
+        {
+            return check;
+        }
+        return NotFound("User not found.");
     }
 
     [HttpPost("login")]
@@ -18,5 +50,25 @@ public class AuthController : ControllerBase
     {
         await Task.Delay(1);
         return Ok("Login");
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<User>> DeactivateUserByIdAsync(int id)
+    {
+        User? check = await _db.Users.FindAsync(id);
+        if (check != null)
+        {
+            try
+            {
+                check.IsActive = false;
+                await _db.SaveChangesAsync();
+                return check;
+            }
+            catch
+            {
+                return StatusCode(500, "Error deactivating user, please try again.");
+            }
+        }
+        return NotFound("User not found.");
     }
 }
--- DataStorage/DBContext.cs
+++ DataStorage/DBContext.cs
@@ -6,5 +6,11 @@ namespace MyApp.DataStorage;
 public class DBContext : DbContext
 {
     public DbSet<User> Users { get; set; }
+
     public DBContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().HasQueryFilter(u => u.IsActive);
+    }
 }
